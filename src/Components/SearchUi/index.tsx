29be5fcfd4d61c6b01c89e8f08ac94460d8388b0
Omiadze <teoomiadze18@gmail.com>
2024-03-28 20:16:37 +0400

    change search placeholder

--- src/Components/SearchUi/index.tsx
+++ src/Components/SearchUi/index.tsx
@@ -4,10 +4,7 @@ const index = ({ onChange, query }) => {
   return (
     <div className="md:ml-8">
       <form className="max-w-md mx-auto ">
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
+        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
           Search
         </label>
         <div className="relative">
@@ -32,7 +29,7 @@ const index = ({ onChange, query }) => {
             type="search"
             id="default-search"
             className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Mockups, Logos..."
+            placeholder="Search"
             required
             onChange={onChange}
             value={query}
