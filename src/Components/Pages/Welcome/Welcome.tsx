5bb9ca635baebee582635621645801062a73c76c
Omiadze <teoomiadze18@gmail.com>
2024-03-29 13:38:44 +0400

    changed SVG in welcome.tsx

--- src/Components/Pages/Welcome/Welcome.tsx
+++ src/Components/Pages/Welcome/Welcome.tsx
@@ -12,7 +12,7 @@ const Welcome = () => {
     navigate('./login')
   }
   return (
-    <div className="bg-lime h-screen flex items-center justify-center font-DM">
+    <div className="bg-lime h-screen flex items-center flex-col justify-center font-DM">
       <div className="flex flex-wrap justify-center gap-8">
         {welcomeCards.map((card) => (
           <button
@@ -22,23 +22,23 @@ const Welcome = () => {
             <p className="text-xl">{card}</p>
           </button>
         ))}
-        <div className="flex items-center hover:cursor-pointer hover:pl-2 text-blue">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-8 h-8"
-            onClick={navToLogin}
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-        </div>
+      </div>
+      <div className=" mt-8 hover:cursor-pointer hover:pl-2 text-blue">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-8 h-8"
+          onClick={navToLogin}
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
       </div>
     </div>
   )
